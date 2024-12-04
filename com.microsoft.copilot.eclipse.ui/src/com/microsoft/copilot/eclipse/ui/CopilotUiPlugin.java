2bcf3f5e23eff122d6b34d8825bcf5a86dc2a948
Sheng Chen <sheche@microsoft.com>
2024-12-04 15:31:11 +0800

    build - Initial commit to setup Repo. (#4)
    * Setup Checkstyle for the project.
    * Add module Core, which stores code that is not related with ui.
    * Add module UI, which stores code that is UI realted, for example SWT
      related code.

---
+++ com.microsoft.copilot.eclipse.ui/src/com/microsoft/copilot/eclipse/ui/CopilotUiPlugin.java
@@ -0,0 +1,21 @@
+package com.microsoft.copilot.eclipse.ui;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Activator class for the Copilot UI plugin.
+ */
+public class CopilotUiPlugin implements BundleActivator {
+
+  @Override
+  public void start(BundleContext context) throws Exception {
+
+  }
+
+  @Override
+  public void stop(BundleContext context) throws Exception {
+
+  }
+
+}
