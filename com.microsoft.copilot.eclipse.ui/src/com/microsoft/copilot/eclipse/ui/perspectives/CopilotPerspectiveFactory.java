30ccba12fb9693324311da293a40776726ca9949
Peter Tao <petertao@microsoft.com>
2025-07-18 15:58:13 +0800

    fix - perspective OnboardingImage disappear after switching light/dark theme & some optimization (#986)

--- com.microsoft.copilot.eclipse.ui/src/com/microsoft/copilot/eclipse/ui/perspectives/CopilotPerspectiveFactory.java
+++ com.microsoft.copilot.eclipse.ui/src/com/microsoft/copilot/eclipse/ui/perspectives/CopilotPerspectiveFactory.java
@@ -5,6 +5,8 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.progress.IProgressConstants;
+import org.eclipse.ui.texteditor.templates.TemplatesView;
 
 import com.microsoft.copilot.eclipse.core.Constants;
 
@@ -18,30 +20,43 @@ public class CopilotPerspectiveFactory implements IPerspectiveFactory {
     // Get the editor area id
     String editorArea = layout.getEditorArea();
 
-    // Put Copilot ChatView to the right of the editor area
+    // Right
     IFolderLayout copilotLayout = layout.createFolder("right", IPageLayout.RIGHT, 0.75f, editorArea);
     copilotLayout.addView(Constants.CHAT_VIEW_ID);
 
-    // Put Project Explorer view on the left side
+    // Left
     IFolderLayout projectExplorerLayout = layout.createFolder("left", IPageLayout.LEFT, 0.25f, editorArea);
     projectExplorerLayout.addView(IPageLayout.ID_PROJECT_EXPLORER);
 
-    // Put Outline view and Mylyn tasks view at the bottom of Project Explorer view
+    // BottomLeft
     IFolderLayout outlineLayout = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, 0.5f, "left");
     outlineLayout.addView(IPageLayout.ID_OUTLINE);
     outlineLayout.addView("org.eclipse.mylyn.tasks.ui.views.tasks");
 
-    // Put Console view at the bottom of the editor area
+    // Bottom
     IFolderLayout consoleLayout = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.8f, editorArea);
     consoleLayout.addView(IPageLayout.ID_PROBLEM_VIEW);
     consoleLayout.addView(IConsoleConstants.ID_CONSOLE_VIEW);
-    consoleLayout.addView(IUIConstants.ID);
-
-    // Set the editor onboarding view
-    layout.setEditorOnboardingText("Open a file or drop files here to open them");
-    layout.setEditorOnboardingImageUri(
-        "platform:/plugin/com.microsoft.copilot.eclipse.ui/icons/copilot_perspective.png");
-    layout.addEditorOnboardingCommandId("com.microsoft.copilot.eclipse.commands.openChatView");
-    layout.addEditorOnboardingCommandId("org.eclipse.ui.window.showKeyAssist");
+    consoleLayout.addView("org.eclipse.tm.terminal.view.ui.TerminalsView");
+    
+    consoleLayout.addPlaceholder(IPageLayout.ID_BOOKMARKS);
+    consoleLayout.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
+    consoleLayout.addPlaceholder("org.eclipse.pde.runtime.LogView");
+    consoleLayout.addPlaceholder("org.eclipse.search.ui.views.SearchView");
+    
+    // 'Window' > 'Show View' shortcuts
+    layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
+    layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+    layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+    layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
+    layout.addShowViewShortcut(IProgressConstants.PROGRESS_VIEW_ID);
+    layout.addShowViewShortcut(IPageLayout.ID_PROJECT_EXPLORER);
+    layout.addShowViewShortcut(TemplatesView.ID);
+    layout.addShowViewShortcut("org.eclipse.tm.terminal.view.ui.TerminalsView");
+    layout.addShowViewShortcut("org.eclipse.pde.runtime.LogView");
+    layout.addShowViewShortcut("org.eclipse.search.ui.views.SearchView");
+    
+    // 'Window' > 'Perspective' > 'Open Perspective' contributions
+    layout.addPerspectiveShortcut("org.eclipse.debug.ui.DebugPerspective");
   }
 }
